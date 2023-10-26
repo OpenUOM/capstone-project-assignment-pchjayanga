7b51fb03a0df1614a9293d7af525149f215ee2cb
pchjayanga <punchihewaj516@gmail.com>
2023-10-26 19:06:14 +0530

    database changes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,15 @@
+const server  = require ("./server.js");
 
+if(process.env.NODE_ENV === "test"){
+  server.listen(4401, () => {
+    console.log(
+        "Capstone Project Backend is running on http://localhost:3000"
+    );
+  });
+}else{
   server.listen(8080, () => {
     console.log(
         "Capstone Project Backend is running on http://localhost:8080"
     );
   });
+}
